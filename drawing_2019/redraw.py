2459a91540a4f12f50a046dbfa1c622c77b7d18c
JDWammes <jdwammes@gmail.com>
2019-11-06 15:54:19 -0500

    Make line drawing more efficient

--- drawing_2019/redraw.py
+++ drawing_2019/redraw.py
@@ -1,6 +1,5 @@
 from psychopy import visual, event, core, monitors
 import pandas as pd
-from numpy import array
 
 monitor_name = "ObiMonitor"
 scnWidth, scnHeight = monitors.Monitor(monitor_name).getSizePix()
@@ -14,6 +13,7 @@ circle = visual.Circle(
     radius=2,
     fillColor=[1] * 3)
 
+line = visual.Line(mywin, lineWidth=5)
 
 def draw(file_name, xLoc):
     """ Takes in filename and column location of x coordinates """
@@ -24,12 +24,13 @@ def draw(file_name, xLoc):
     
     for i in range(length):  # loop through rows of points
         
-        firstPoint = array([df[xLoc][i], df[xLoc + 1][i]])  # xLoc + 1 = yLoc ==> column location of y coordinates
-        secondPoint = array([df[xLoc][i + 1], df[xLoc + 1][i + 1]])
+        firstPoint = tuple([df[xLoc][i], df[xLoc + 1][i]])  # xLoc + 1 = yLoc ==> column location of y coordinates
+        secondPoint = tuple([df[xLoc][i + 1], df[xLoc + 1][i + 1]])
         
         if df[xLoc][i] != "None" and df[xLoc][i + 1] != "None":
             # connect the points of the lines being drawn
-            line = visual.Line(mywin, start=tuple(firstPoint), end=tuple(secondPoint), lineWidth=5)
+            line.start = firstPoint
+            line.end = secondPoint
             line.draw()
             
         elif df[xLoc][i] != "None":  # need one point to draw circle (similar to drawing.py implementation)
