8535b6ac39167e7e04adb4f572e7819a6947d9dd
JDWammes <jdwammes@gmail.com>
2019-11-06 15:49:58 -0500

    updated redraw to show picture

--- drawing_2019/redraw.py
+++ drawing_2019/redraw.py
@@ -1,45 +1,43 @@
 from psychopy import visual, event, core, monitors
 import pandas as pd
-from numpy import array 
+from numpy import array
 
 monitor_name = "ObiMonitor"
 scnWidth, scnHeight = monitors.Monitor(monitor_name).getSizePix()
 
 mywin = visual.Window([scnWidth-10, scnHeight-10], units="pix",
-                  monitor=monitor_name, fullscr=False, waitBlanking=False, allowGUI=True)
+                      monitor=monitor_name, fullscr=True, waitBlanking=False, allowGUI=False)
                   
 circle = visual.Circle(
-    win = mywin,
-    units = "pix",
-    radius = 2,
-    fillColor = [1] * 3,
-    #lineColor = [1] * 3,
-    # edges = 128
-) # define circle with certain radius and edge that will be drawn on screen
+    win=mywin,
+    units="pix",
+    radius=2,
+    fillColor=[1] * 3)
 
 
 def draw(file_name, xLoc):
     """ Takes in filename and column location of x coordinates """
     
     df = pd.read_excel(file_name)
-    df = df.fillna("None") # fill all nan values with "None" string
+    df = df.fillna("None")  # fill all nan values with "None" string
     length = len(df) - 1
     
-    for i in range(length): # loop through rows of points 
+    for i in range(length):  # loop through rows of points
         
-        firstPoint = array([df[xLoc][i], df[xLoc + 1][i]]) # xLoc + 1 = yLoc ==> column location of y coordinates 
+        firstPoint = array([df[xLoc][i], df[xLoc + 1][i]])  # xLoc + 1 = yLoc ==> column location of y coordinates
         secondPoint = array([df[xLoc][i + 1], df[xLoc + 1][i + 1]])
         
         if df[xLoc][i] != "None" and df[xLoc][i + 1] != "None":
-            line = visual.Line(mywin, start = tuple(firstPoint), end = tuple(secondPoint), lineWidth=5) # connect the points of the lines being drawn
+            # connect the points of the lines being drawn
+            line = visual.Line(mywin, start=tuple(firstPoint), end=tuple(secondPoint), lineWidth=5)
             line.draw()
             
-        elif df[xLoc][i] != "None": # need one point to draw circle (similar to drawing.py implementation)
+        elif df[xLoc][i] != "None":  # need one point to draw circle (similar to drawing.py implementation)
             circle.pos = firstPoint
             circle.draw()
-                 
+
+        mywin.flip(clearBuffer=False)
+
             
 if __name__ == "__main__":
-    draw("mouseClicks.xlsx", 0)
-        
-        
+    draw("mouseClicks.xlsx", 3)
