037df379a274e63cbd019391eea991d1ad491c32
JDWammes <jdwammes@gmail.com>
2019-11-06 16:34:48 -0500

    a few more minor changes to redraw in attempts to reduce flickering

--- drawing_2019/redraw.py
+++ drawing_2019/redraw.py
@@ -1,7 +1,7 @@
 from psychopy import visual, event, core, monitors
 import pandas as pd
 
-monitor_name = "ObiMonitor"
+monitor_name = "MyBig"
 scnWidth, scnHeight = monitors.Monitor(monitor_name).getSizePix()
 
 mywin = visual.Window([scnWidth-10, scnHeight-10], units="pix",
@@ -11,18 +11,24 @@ circle = visual.Circle(
     win=mywin,
     units="pix",
     radius=2,
-    fillColor=[1] * 3)
+    fillColor=[-1, -1, -1],
+    lineWidth=0)
 
-line = visual.Line(mywin, lineWidth=5)
+line = visual.Line(mywin, lineColor=[-1, -1, -1], lineWidth=5)
+
 
 def draw(file_name, xLoc):
     """ Takes in filename and column location of x coordinates """
+
+    drawLine = False
+    drawCircle = False
+    i = 0
     
     df = pd.read_excel(file_name)
     df = df.fillna("None")  # fill all nan values with "None" string
     length = len(df) - 1
     
-    for i in range(length):  # loop through rows of points
+    while i < length:  # loop through rows of points
         
         firstPoint = tuple([df[xLoc][i], df[xLoc + 1][i]])  # xLoc + 1 = yLoc ==> column location of y coordinates
         secondPoint = tuple([df[xLoc][i + 1], df[xLoc + 1][i + 1]])
@@ -31,13 +37,21 @@ def draw(file_name, xLoc):
             # connect the points of the lines being drawn
             line.start = firstPoint
             line.end = secondPoint
-            line.draw()
+            drawLine = True
             
-        elif df[xLoc][i] != "None":  # need one point to draw circle (similar to drawing.py implementation)
+        if df[xLoc][i] != "None":  # need one point to draw circle (similar to drawing.py implementation)
             circle.pos = firstPoint
+            drawCircle = True
+
+        if drawCircle:
             circle.draw()
+        if drawLine:
+            line.draw()
 
         mywin.flip(clearBuffer=False)
+        i += 1
+    for i in range(5):
+        mywin.flip(clearBuffer=True)
 
             
 if __name__ == "__main__":
